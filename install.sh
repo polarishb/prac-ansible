629b298596dec38a1e502e9b7fce63d5d872aa5b
polarishb <polaris.hb03@gmail.com>
2023-05-28 02:31:30 +0900

    update-code

--- install.sh
+++ install.sh
@@ -4,12 +4,27 @@ echo "Start install Kubernetes"
 
 ansible-playbook configure-ansible.yml
 ansible-playbook set-hosts.yml
+
+# 모듈 설치
+var=$(ansible-galaxy collection list | grep community.crypto | awk {'print $1'})
+if ! [ $var == "community.crypto" ]; then
+    ansible-galaxy collection install community.crypto
+fi
+var=$(ansible-galaxy collection list | grep community.general | awk {'print $1'})
+if ! [ $var == "community.general" ]; then
+    ansible-galaxy collection install community.general
+fi
+var=$(ansible-galaxy collection list | grep ansible.posix | awk {'print $1'})
+if ! [ $var == "ansible.posix" ]; then
+    ansible-galaxy collection install ansible.posix
+fi
+
+
+ansible-galaxy collection install ansible.posix
+ansible-galaxy collection install community.general
+
 ansible-playbook install-kubernetes.yml
 ansible-playbook install-haproxy.yml
-
-# Kubernetes Cluster 생성
-ansible-playbook init-master.yml
-scp root@k8s-master01:~/initial_ /sources/
 
 ## Kubectl 설정
 if [ ! -d "/source" ]; then
@@ -18,6 +33,9 @@ if [ ! -d "/source" ]; then
 else
     echo "/sources already exists"
 fi
+
+# Kubernetes Cluster 생성 및 연결
+ansible-playbook init-master.yml
 
 if [ ! -f "/sources/admin.conf "]; then
     scp root@k8s-master01:/etc/kubernetes/admin.conf /sources/
