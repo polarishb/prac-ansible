e961219b37b75fa673e580a14a9f8358cf2700d9
polarishb <37509306+polarishb@users.noreply.github.com>
2023-05-30 15:11:40 +0900

    update

---
+++ init_ansible.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+echo "Initialize Ansible Server"
+
+# 모듈 설치 ( Ansible 2.10 or above)
+#var=$(ansible-galaxy collection list | grep -o community.crypto)
+#if ! [ $var == "community.crypto" ]; then
+#    ansible-galaxy collection install community.crypto
+#fi
+#var=$(ansible-galaxy collection list | grep -o community.general)
+#if ! [ $var == "community.general" ]; then
+#    ansible-galaxy collection install community.general
+#fi
+#var=$(ansible-galaxy collection list | grep -o ansible.posix)
+#if ! [ $var == "ansible.posix" ]; then
+#    ansible-galaxy collection install ansible.posix
+#fi
+
+# 모듈 설치 ( Ansible 2.9 )
+loc=.ansible/collections/ansible_collections/
+if [ ! -d $HOME/$loc/community/general ]; then
+   ansible-galaxy collection install community.general
+else
+    echo "Collection Communitiy General already installed."
+fi
+if [ ! -d $HOME/$loc/community/crypto ]; then
+   ansible-galaxy collection install community.crypto
+else
+    echo "Collection Communitiy Crypto already installed."
+fi
+if [ ! -d $HOME/$loc/ansible/posix ]; then
+   ansible-galaxy collection install ansible.posix
+else
+    echo "Collection Ansible Posix already installed."
+fi
+
+# Ansible Host 설정
+ansible-playbook /sources/configure-ansible.yml
+ansible-playbook /sources/pre-install.yml -k
---
+++ install_k8s.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo "Start install Kubernetes"
+
+# 쿠버네티스 클러스터 hosts, ssh 설정
+ansible-playbook /sources/init-k8s-cluster.yml
+
+# 쿠버네티스 클러스터 환경 구축
+ansible-playbook /sources/install-kubernetes.yml
+ansible-playbook /sources/install-haproxy.yml
+
+# Kubernetes Cluster 생성 및 연결
+ansible-playbook /sources/init-cluster.yml
+ansible-playbook /sources/config-cluster.yml
+
+# Join Token 추출
+sed -n '/^\s.*kubeadm/,/control-plane/p' /sources/initial_log > /sources/Master
+sed -i 's/\\//g' /sources/Master
+sed -n '/^kubeadm/,/discovery/p' /sources/initial_log > /sources/Worker
+sed -i 's/\\//g' /sources/Worker
+
+# 노드 Join
+ansible-playbook /sources/init-master.yml
+ansible-playbook /sources/init-worker.yml
+
+# CNI 설치
+curl https://docs.projectcalico.org/archive/v3.17/manifests/calico.yaml -O /sources/ --insecure
+ansible-playbook /sources/set-cni.yml
+
+# 스토리지 서버 마운트
+ansible-playbook /sources/mount-nfs.yml
+
+echo "Kubernetes Install & Configuration Finished"
