bf3fe2e4eab12536ac56123d2e49686d201dfbc0
polarishb <polaris.hb03@gmail.com>
2023-06-05 14:36:52 +0900

    v0.1.0_Release

--- install_metallb.sh
+++ install_metallb.sh
@@ -7,4 +7,5 @@ kubectl apply -f - -n kube-system
 
 # MetalLB 설치
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.9/config/manifests/metallb-native.yaml
+sleep 60
 kubectl apply -f $HOME/IPPool.yml
