17ee1d33c13c3d3be94e76e2d8a05603d97e9bd3
polarishb <polaris.hb03@gmail.com>
2023-06-05 12:05:51 +0900

    bug fix

--- install_metrics_server.sh
+++ install_metrics_server.sh
@@ -3,5 +3,5 @@
 kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
 
 kubectl get deployments.apps metrics-server -n kube-system -o yaml | \
-sed -e "s/- args:/- args:\n        - --kubelet-insecure-tls\n        - --kubelet-preferred-address-type=InternalIP/" | \
+sed -e "s/- args:/- args:\n        - --kubelet-insecure-tls/" | \
 kubectl apply -f - -n kube-system
